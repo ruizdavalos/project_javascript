0695563fb44ff2e01fa101f863af04721db87f83
ruizdavalos <ruizdavalos@gmail.com>
2022-06-24 22:23:33 -0300

    Metodo pomodor terminado, listo para su uso.

--- 1-Pomodoro/main.js
+++ 1-Pomodoro/main.js
@@ -9,6 +9,13 @@ const bAdd= document.querySelector('#bAdd');
 const itTask= document.querySelector('#itTask');
 const form= document.querySelector('#form');
 
+const taskName= document.querySelector("#time #taskName");
+
+renderTime();
+renderTask();
+
+
+
 form.addEventListener('submit',e=>{ //hago una funcion anonima
     e.preventDefault(); //con esto hacemos anulamos su funcionamiento nativo, por lo que no va a enviar el formulario
     if(itTask.value !==''){
@@ -45,5 +52,80 @@ function renderTask(){
 
 
     const tasksContainer = document.querySelector('#tasks');
-    tasksContainer.innerHTML = html.join(" ");
+    tasksContainer.innerHTML = html.join("");
+
+    const startButtons= document.querySelectorAll(".task .start-button");
+    
+    startButtons.forEach((button)=> {
+        button.addEventListener("click", (e)=> {
+            if(!timer) {
+                const id= button.getAttribute("data-id");
+                startButtonHandler(id);
+                button.textContent= "In progress...";
+            }
+        })
+    })
+
+
+}
+
+
+function startButtonHandler (id){
+    time= 5;
+    current= id;
+    const taskIndex= tasks.findIndex(task=> task.id===id);
+
+    taskName.textContent= tasks[taskIndex].title;
+    renderTime();
+
+    timer= setInterval(()=>{
+        timeHandler(id)
+    },1000);
+}
+
+function timeHandler(id){
+    time--;
+    renderTime();
+    if(time==0){
+        clearInterval(timer);
+        markCompleted(id);
+        timer=null;
+        renderTask();
+        startBreak();
+    }
+}
+
+function startBreak(){
+    time= 3;
+    taskName.textContent = 'Break';
+    renderTime()
+    timerBreak= setInterval(()=> {
+        timerBreakHandler();
+    },1000)
+}
+
+function timerBreakHandler(){
+    time--;
+    renderTime();
+    if(time===0){
+        clearInterval(timerBreak);
+        current=null;
+        timerBreak=null
+        taskName.textContent="";
+        renderTask();
+
+    }
+}
+
+
+function renderTime(){
+    const timeDiv= document.querySelector('#time #value');
+    const minutes= parseInt(time/60);
+    const seconds= parseInt(time%60);
+    timeDiv.textContent=`${minutes<10 ? '0':""}${minutes}:${seconds<10 ? '0':""}${seconds}`;
+}
+
+function markCompleted(id) {
+    const taskIndex= tasks.findIndex((task)=> task.id===id);
+    tasks[taskIndex].completed=true;
 }
